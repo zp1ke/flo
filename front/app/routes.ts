e7a86d895fea8c633bb1b45ff1232a54d86fa701
Zp1k_e <zp1ke@proton.me>
2025-05-18 13:58:40 -0500

    chore: update .gitignore to include .idea directory and standardize quotes in routes.ts

--- front/app/routes.ts
+++ front/app/routes.ts
@@ -1,7 +1,7 @@
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import { type RouteConfig, index, layout, route } from '@react-router/dev/routes';
 
 export default [
-  index("routes/home.tsx"),
+  index('routes/home.tsx'),
   layout('components/auth-layout.tsx', [
     route('dashboard', 'routes/dashboard.tsx'),
   ]),
