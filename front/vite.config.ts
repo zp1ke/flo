771d73628e81004de5cc88af73e89830d70ca71f
Zp1k_e <zp1ke@proton.me>
2025-05-20 08:55:30 -0500

    fix: add allowedHosts configuration for Vite server

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
+    allowedHosts: ['doberman-brave-nicely.ngrok-free.app'],
     host: '0.0.0.0',
     port: 3000,
   },
