2561b54296723906a4a29a46b3de1b2b1a3c8e8f
Zp1k_e <zp1ke@proton.me>
2025-06-02 17:36:07 -0500

    feat(email): add new email templates for user verification and onboarding; update .gitignore and package files

--- email/render.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsx_runtime_1 = require("react/jsx-runtime");
-var render_1 = require("@react-email/render");
-var user_verification_1 = require("~/user-verification");
-var html = await (0, render_1.pretty)(await (0, render_1.render)((0, jsx_runtime_1.jsx)(user_verification_1.default, {})));
-console.log(html);
--- email/render.tsx
+++
@@ -1,6 +0,0 @@
-import { render, pretty } from '@react-email/render';
-import UserVerification from '~/user-verification';
-
-const html = await pretty(await render(<UserVerification />));
-
-console.log(html);
