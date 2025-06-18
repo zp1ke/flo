8a1aa822793a06cf08210a4a5207711ef3a29fab
Zp1k_e <zp1ke@proton.me>
2025-06-17 22:09:18 -0500

    feat: enhance GeneralBanner component to navigate to settings on user verification prompt

--- front/app/components/layout/general-banner.tsx
+++ front/app/components/layout/general-banner.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router';
 import useUserStore from '~/store/user-store';
 import { TypingText } from '../animate-ui/text/typing';
 
@@ -6,12 +7,15 @@ export default function GeneralBanner() {
   const user = useUserStore((state) => state.user);
   const { t } = useTranslation();
 
+  const navigate = useNavigate();
+
   if (user && !user?.verified) {
     return (
       <TypingText
-        className="text-destructive text-sm font-medium"
+        className="cursor-pointer text-destructive text-sm font-medium"
         text={t('verify.userNotVerified')}
         loop={true}
+        onClick={() => navigate('/settings')}
       />
     );
   }
