86b418244f73229d37e8db59634283b304fb228c
Zp1k_e <zp1ke@proton.me>
2025-07-20 21:51:34 -0500

    backend- feat: add timezone configuration for application and docker compose

--- backend/flo-api/src/main/java/com/zp1ke/flo/api/FloApiApplication.java
+++ backend/flo-api/src/main/java/com/zp1ke/flo/api/FloApiApplication.java
@@ -1,11 +1,23 @@
 package com.zp1ke.flo.api;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.zp1ke.flo")
 public class FloApiApplication {
+
+    @Value("${spring.application.timezone:UTC}")
+    private String timeZone;
+
     public static void main(String[] args) {
         SpringApplication.run(FloApiApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
     }
 }
